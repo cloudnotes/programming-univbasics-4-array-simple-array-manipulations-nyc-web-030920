4db8fb0c3e244fc938583ba46077bfebe1801b9a
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-02-29 22:11:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,5 +19,15 @@ def using_shift(array)
 end
 
 def shift_with_args(array)
-  
+  array.shift(2)
 end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(element, 4)
+end
+
+def using_uniq(array)
